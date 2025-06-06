bdb0e6fcea18898bf873f9d83169bcdd70898a2d
Nasywa Kamila <nasywakamila3769@gmail.com>
2025-06-06 20:50:55 +0700

    login page + app.jsx edited

--- front_end/admin/adminWeb/src/App.jsx
+++ front_end/admin/adminWeb/src/App.jsx
@@ -1,21 +1,41 @@
-import { useState } from 'react'
-import React from 'react'
-import './App.css'
-import Navbar from './components/navbar'
-import Sidebar from './components/Sidebar'
+import { useState } from 'react';
+import React from 'react';
+import './App.css';
+import Login from './pages/Login';
+import Listitems from './pages/ListItems';
+import Add from './pages/Add';
+import Order from './pages/Order';
+import Navbar from './components/navbar';
+import Sidebar from './components/Sidebar';
+import { Route, Routes, useLocation } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const location = useLocation();
+
+  // Cek apakah di halaman login
+  const isLoginPage = location.pathname === '/login';
 
   return (
     <>
-    <div className='px-4 sm:px=[5vw] md:px=[7vw] lg:px-[9vw]'>
-       <Navbar/>
-       <Sidebar/>
-    </div>
-     
+      {/* Jangan tampilkan navbar & sidebar di halaman login */}
+      {!isLoginPage && <Navbar />}
+      
+      <div className="flex">
+        {!isLoginPage && <Sidebar />}
+        
+        {/* Main content area */}
+        <div className={`flex-1 ${!isLoginPage ? 'ml-0' : ''}`}>
+          <Routes>
+            <Route path='/login' element={<Login />} />
+            <Route path='/listitems' element={<Listitems />} />
+            <Route path='/additems' element={<Add />} />
+            <Route path='/orders' element={<Order />} />
+          </Routes>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
