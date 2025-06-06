7e5485b2379d9b029eadd505a5e22179e753120e
VeironVaya <veironvayayarief41@gmail.com>
2025-06-06 18:11:12 +0700

    fetch and delete done

--- back_end/springboot/src/main/java/com/example/springboot/controller/ProductController.java
+++ back_end/springboot/src/main/java/com/example/springboot/controller/ProductController.java
@@ -1,20 +1,20 @@
 package com.example.springboot.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PatchMapping;
-
 
 import com.example.springboot.dto.ProductDto;
 import com.example.springboot.entity.Product;
 import com.example.springboot.service.ProductService;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/products")
