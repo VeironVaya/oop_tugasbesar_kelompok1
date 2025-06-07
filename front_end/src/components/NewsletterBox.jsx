d3ca5a55e3b78455cf77e5e9e55a8c64722d3a6a
yolandarch <yolandachrysti@gmail.com>
2025-06-07 13:10:04 +0700

    70% skelton fe customer

---
+++ front_end/src/components/NewsletterBox.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { assets } from "../assets/assets";
+import { Link } from "react-router-dom";
+
+
+const NewsletterBox = () => {
+  // return (
+  //   <Link to="/collection" className="w-full">
+  //     <div className="w-full h-80 mb-20 bg-[#f3f3f3] md:bg-transparent relative font-titleFont">
+  //       <Image
+  //         className="w-full h-full object-cover hidden md:inline-block"
+  //         imgSrc={assets.p_img1}
+  //         alt="Product of The Year"
+  //       />
+  //       <div className="w-full md:w-2/3 xl:w-1/2 h-80 absolute px-4 md:px-0 top-0 right-0 flex flex-col items-start gap-6 justify-center">
+  //         <h1 className="text-3xl font-semibold text-primeColor">
+  //           Special Offer: Product of The Year
+  //         </h1>
+  //         <p className="text-base font-normal text-primeColor max-w-[600px] mr-4">
+  //           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat
+  //           cupiditate modi amet! Facilis, aperiam quaerat.
+  //         </p>
+  //         <ShopNow />
+  //       </div>
+  //     </div>
+  //   </Link>
+  // );
+};
+
+export default NewsletterBox
