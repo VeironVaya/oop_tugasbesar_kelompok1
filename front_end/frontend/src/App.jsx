85015f61b6a88e22be76355458de0f3873b12c71
Nasywa Kamila <nasywakamila3769@gmail.com>
2025-06-13 17:13:57 +0700

    login jadi

--- front_end/frontend/src/App.jsx
+++ front_end/frontend/src/App.jsx
@@ -7,7 +7,7 @@ import Contact from "./pages/Contact";
 import Product from "./pages/Product";
 import Cart from "./pages/Cart";
 import Login from "./pages/Login";
-import PlaceOrder from "./pages/PlaceOrder";
+
 import Orders from "./pages/Orders";
 import Navbar from "./components/Navbar";
 import MyFavorite from "./pages/MyFavorite";
@@ -28,7 +28,7 @@ const App = () => {
         <Route path="/cart" element={<Cart />} />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/place-order" element={<PlaceOrder />} />
+        
         <Route path="/orders" element={<Orders />} />
         <Route path="/regist" element={<Regist />} />
       </Routes>
